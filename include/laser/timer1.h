42c3d37982b5526873dcc92320654419faa47198
Michael Free <11mikefree@gmail.com>
2018-03-26 13:49:08 -0500

    Implement input capture edge selection for laser

--- include/laser/timer1.h
+++ include/laser/timer1.h
@@ -65,6 +65,8 @@ timer1_disable_overflow ( void )
 static inline void __attribute__ (( __always_inline__ ))
 timer1_enable_capture ( void )
 {
+  TCCR1B |= _BV ( ICNC1 ) ;
+  TCCR1B |= _BV ( ICES1 ) ;
   TIMSK1 |= _BV ( ICIE1 ) ;
 }
 
@@ -72,6 +74,26 @@ static inline void __attribute__ (( __always_inline__ ))
 timer1_disable_capture ( void )
 {
   TIMSK1 &= ~ ( _BV ( ICIE1 ) ) ;
+  TCCR1B &= ~ ( _BV ( ICES1 ) ) ;
+  TCCR1B &= ~ ( _BV ( ICNC1 ) ) ;
+}
+
+static inline void __attribute__ (( __always_inline__ ))
+timer1_capture_falling ( void )
+{
+  TCCR1B &= ~ ( _BV ( ICES1 ) ) ;
+}
+
+static inline void __attribute__ (( __always_inline__ ))
+timer1_capture_rising ( void )
+{
+  TCCR1B |= _BV ( ICES1 ) ;
+}
+
+static inline void __attribute__ (( __always_inline__ ))
+timer1_capture_edge_swap ( void )
+{
+  TCCR1B ^= _BV ( ICES1 ) ;
 }
 
 #endif
