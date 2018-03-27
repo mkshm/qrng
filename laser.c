ddc551077d354ec47e143a2b678616b98e04ed33
Michael Free <11mikefree@gmail.com>
2018-03-27 14:09:19 -0500

    Actually start implementing things for the laser
    Not at all finished. Don't read this code.

--- laser.c
+++ laser.c
@@ -1,53 +1,46 @@
 
 #include <util/delay.h>
 
-#include <debug/backtrace.h>
-
 #include <laser/timer1.h>
 #include <laser/interrupt.h>
-#include <laser/pins.h>
 #include <laser/rotary.h>
 
 int
 main ( void )
 {
-  unsigned char pin [ 6 ] ;
-  unsigned char flag ;
+  disable_interrupts (  ) ;
+  
+  _delay_ms ( 50 ) ; // Allow devices voltage to come rise
 
-  _delay_ms ( 50 ) ;
-
+  selector_init (  ) ;
+  interlock_init (  ) ;
+  rotary_init (  ) ;
   timer1_init (  ) ;
+  
   timer1_pwm_on (  ) ;
   timer1_freq ( 2000U ) ;
-  timer1_duty (  500U ) ;
+  timer1_duty (    5U ) ;
 
-  rotary_init (  ) ;
-
-  disable_interrupts (  ) ;
-/*
   while ( 1 )
   {
-    pin [ 0 ] = PINB ;
-    pin [ 1 ] = PINC ;
-    pin [ 2 ] = PIND ;
-
-    enable_interrupts (  ) ;
-
-    //magic
-
-    pin [ 3 ] = pin [ 0 ] ;
-    pin [ 4 ] = pin [ 1 ] ;
-    pin [ 5 ] = pin [ 2 ] ;
-
-    disable_interrupts (  ) ;
+    if ( interlock_open (  ) )
+    {
+      prev = timer1_duty ( 5u ) ;
+      
+      while ( interlock_open (  ) ||  ) ;
+      
+      timer1_duty ( prev ) ;
+    }
+    
+    select = selector_ ;
+    
+    switch (  )
+    {
+      
+    }
+    
+    rotary_step (  ) ;
   }
-*/
-  //backtrace (  ) ;
-
-  timer1_pwm_off (  ) ;
-  timer1_stop (  ) ;
-
-  sleep_mode (  ) ;
 
   return 0 ;
 }
