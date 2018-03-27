b477bc1211f1a12a9a20a711b8713e876ae48326
Michael Free <11mikefree@gmail.com>
2018-03-27 14:07:41 -0500

    Actually update lock.h this time

--- include/qrng/lock.h
+++ include/qrng/lock.h
@@ -1,23 +1,25 @@
 
-#ifndef __UTIL_SERIAL_H__
-#define __UTIL_SERIAL_H__
+#ifndef __UTIL_LOCK_H__
+#define __UTIL_LOCK_H__
 
 #include <stdbool.h>
 
 #define STATIC_INLINE static inline __attribute__ (( __always_inline__ , __hot__ ))
 
 STATIC_INLINE unsigned char
-lock_acquire ( unsigned char * const restrict next , const unsigned char * const restrict serv )
+lock_acquire ( volatile unsigned char * const restrict next ,
+         const volatile unsigned char * const restrict serv )
 {
-  unsigned char tikt = * next ++ ;
-  while ( * serv != tikt ) ;
+  unsigned char tikt = ( * next ) ++ ;
+  while ( ( * serv ) - 1 != tikt ) ;
   return tikt ;
 }
 
 STATIC_INLINE void
-lock_release ( unsigned char * const restrict serv )
+lock_release ( volatile unsigned char * const restrict serv ,
+         const volatile unsigned char * const restrict next )
 {
-  * serv ++ ;
+  ( * serv ) = ( * next ) ;
 }
 
 #undef STATIC_INLINE
