05a388823df56fdab7fd706cfca4b2f15bd26990
Michael Free <11mikefree@gmail.com>
2018-03-27 14:14:44 -0500

    Implement rotary encoder logic

--- include/laser/rotary.h
+++ include/laser/rotary.h
@@ -3,27 +3,25 @@
 #define __UTIL_ROTARY_H__
 
 #include <avr/io.h>
-#include <avr/sleep.h>
-#include <avr/interrupt.h>
-#include <avr/pgmspace.h>
 #include <avr/sfr_defs.h>
-#include <avr/eeprom.h>
 
 static inline void __attribute__ (( __always_inline__ ))
 rotary_init ( void )
 {
-  DDRB  &= ~ ( _BV ( DD2 ) | _BV ( DD3 ) ) ;
-  PORTB |=     _BV ( PB2 ) | _BV ( PB3 ) ;
+  DDRB  &= ~ ( _BV ( DD2 ) | _BV ( DD3 ) ) ; /* Set pins 2 and 3 to INPUT */
+  PORTB |=     _BV ( PB2 ) | _BV ( PB3 ) ;   /* Enable pull-up resistor */
 }
 
 static inline signed char __attribute__ (( __always_inline__ ))
 rotary_step ( void )
 {
   signed char ret ;
-  while ( ( ret = PINB | _BV ( PB2 ) | _BV ( PB3 ) ) )
-  {
-  }
-  return ret ;
+  signed char lhs , rhs ;
+  
+  lhs = ( PINB & _BV ( PB2 ) ) >> PB2 ; /* pin 2 is selected here to be left */
+  rhs = ( PINB & _BV ( PB3 ) ) >> PB3 ; /* pin 3 is selected here to be right */
+  
+  return rhs - lhs ; /* going right is positive 1 , 0 is no change , -1 is left */
 }
 
 #endif
