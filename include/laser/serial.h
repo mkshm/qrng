33481c59bb4ba21ff8acc3041f6b8c5770ddbac6
Michael Free <11mikefree@gmail.com>
2018-04-02 13:46:14 -0500

    Start converting laser code to new design
    Starting with USART.

--- include/laser/serial.h
+++ include/laser/serial.h
@@ -5,28 +5,44 @@
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
 
-#define STATIC_INLINE static inline void __attribute__ (( __always_inline__ ))
+#include <stdbool.h>
 
-STATIC_INLINE
+#define STATIC_INLINE static inline __attribute__ (( __always_inline__ ))
+
+STATIC_INLINE void
 serial_init ( void )
 {                                              // mult = 16 for 1x, 8 for 2x ; f_osc = clock frequency
   UBRR0   = 0u ;                               // Select baud using: baud = f_osc / ( mult * ( UBBR0 + 1 ) )
   UCSR0A |= _BV ( U2X0 ) ;                     // 2x Speed Enable ( changes multiplier above )
-  UCSR0C |= _BV ( UCSZ00 ) | _BV ( UCSZ01 ) ;  // 8 bit Output Frame
-  UCSR0B |= _BV ( TXEN0 ) ;                    // Enable Transmitted Mode
+  UCSR0C |= _BV ( UCSZ00 ) | _BV ( UCSZ01 ) ;  // 8 bit
+  UCSR0C |= _BV ( UPM01  ) | _BV ( USBS0  ) ;  // 1 stop bit + even parity
+  UCSR0B |= _BV ( TXEN0  ) | _BV ( RXEN0  ) ;  // Enable transmitter and reciver
+  UCSR0B |= _BV ( TXCIE0 ) | _BV ( RXCIE0 ) ;  // Enable interrupts
+  DDRD   |= _BV ( DDD1 ) ;
 }
 
-STATIC_INLINE
-serial_loop ( const volatile unsigned char * const restrict buff ,
-                    volatile unsigned char * const restrict head ,
-                    volatile unsigned char * const restrict tail )
+STATIC_INLINE void
+serial_wait ( void )
 {
-  while ( 1 )
-  {
-    while ( * tail != * head ) ;              // Spin when tail == head, the buffer is full
-    while ( ! ( UCSR0A & _BV ( UDRE0 ) ) ) ;  // Check for an empty buffer by checking the UDRE0 bit
-    UDR0 = buff [ ( * tail ) ++ ] ;           // Write to the empty buffer. automatically send the data written.
-  }
+  while ( ! ( UCSR0A & _BV ( UDRE0 ) ) ) ;  // Check for an empty buffer by checking the UDRE0 bit
+}
+
+STATIC_INLINE bool
+serial_empty ( void )
+{
+  return 0 != ( UCSR0A & _BV ( UDRE0 ) ) ;
+}
+
+STATIC_INLINE void
+serial_send ( unsigned char byte )
+{
+  UDR0 = byte ; // Write to the empty buffer. automatically send the data written.
+}
+
+STATIC_INLINE unsigned char
+serial_recv ( void )
+{
+  return UDR0 ; // Read full buffer
 }
 
 #undef STATIC_INLINE
