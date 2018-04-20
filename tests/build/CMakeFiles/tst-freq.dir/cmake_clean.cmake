c8802b227282b624b4ad686205566dff8f96c231
Michael Free <11mikefree@gmail.com>
2018-04-20 14:37:14 -0500

    fudge the numbers...

---
+++ tests/build/CMakeFiles/tst-freq.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tst-freq.dir/tst-freq.c.o"
+  "tst-freq.pdb"
+  "tst-freq"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tst-freq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- tests/tst-monte-3d.c
+++ tests/tst-monte-3d.c
@@ -1,4 +1,5 @@
 
+#include <float.h>
 #include <math.h>
 
 #include <tst-common.h>
@@ -6,6 +7,7 @@
 int
 test ( int fd )
 {
+  double x , y , z ;
   double mag_sqr ;
   double i = 0.0 , n = 0.0 ;
 
@@ -26,17 +28,23 @@ test ( int fd )
       if ( FP_NORMAL != fpclassify ( buff [ tmp * 3 + 1 ] ) ) continue ;
       if ( FP_NORMAL != fpclassify ( buff [ tmp * 3 + 2 ] ) ) continue ;
 
-      mag_sqr  = buff [ tmp * 3 + 0 ] * buff [ tmp * 3 + 0 ] ;
-      mag_sqr += buff [ tmp * 3 + 1 ] * buff [ tmp * 3 + 1 ] ;
-      mag_sqr += buff [ tmp * 3 + 2 ] * buff [ tmp * 3 + 2 ] ;
+      i += 1.0 ;
 
-      if ( 1 >= mag_sqr ) n += 1.0 ; else i += 1.0 ;
+      x = 2 * buff [ tmp * 3 + 0 ] / sqrt ( DBL_MAX ) ;
+      y = 2 * buff [ tmp * 3 + 1 ] / sqrt ( DBL_MAX ) ;
+      z = 2 * buff [ tmp * 3 + 2 ] / sqrt ( DBL_MAX ) ;
+
+      mag_sqr  = x * x ;
+      mag_sqr += y * y ;
+      mag_sqr += z * z ;
+
+      if ( 1 >= mag_sqr ) n += 1.0 ;
     }
   }
 
   argp_verbose ( "[tst-monte-3d]" , "The total inside  is:" , n ) ;
-  argp_verbose ( "[tst-monte-3d]" , "The total         is:" , n + i ) ;
-  argp_verbose ( "[tst-monte-3d]" , "The volume is:" , 8 * ( n /( n + i )) ) ;
+  argp_verbose ( "[tst-monte-3d]" , "The total         is:" , i ) ;
+  argp_verbose ( "[tst-monte-3d]" , "The volume is:" , 8 * ( n / i ) ) ;
 
   if ( NULL != buff ) free ( buff ) ;
 
